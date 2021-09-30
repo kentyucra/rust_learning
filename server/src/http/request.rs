7c19bd6797104cbb62e370e45a9f1b8a63304584
Kent Yucra <mckkentmwk@gmail.com>
2021-09-29 22:38:46 -0500

    Learn about traits

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -4,9 +4,132 @@ HEADERS \r\n
 BODY
 */
 
-use super::method::Method;
+use super::method::{Method, MethodError};
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Debug, Result as FmtResult, Formatter};
+use std::str::{self, Utf8Error};
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    // GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // // First way to handle error (Utf8Error)
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {},
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
+        
+        // // Second way to handle error (Utf8Error)
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(request) => {},
+        //     Err(e) => return Err(e),
+        // }
+
+        // // Third way to handle error (Utf8Error)
+        // let request= str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+        // Fourth way to handle error (Utf8Error) **cleaner way**
+        let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+        // // First form to unwrap the value from find function
+        // let mut query_string = None;
+        // match path.find('?') {
+        //     Some(i) => {
+        //         query_string = Some(&path[i+1..]);
+        //         path = &path[..i];
+        //     }
+        //     None => {}
+        // }
+
+        // // Second form to unwrap the value from find function
+        // let q = path.find('?');
+        // if q.is_some() {
+        //     let i = q.unwrap();
+        //     query_string = Some(&path[i+1..]);
+        //     path = &path[..i];
+        // }
+
+        // Best form to unwrap the value from find function
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]);
+            path = &path[..i];
+        }
+
+
+        unimplemented!()
+    }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i+1..]));
+        }
+    }
+    return None;
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Enconding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        return Self::InvalidMethod;
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        return Self::InvalidEncoding;
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {
+
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::net::TcpListener;
 use std::io::Read;
 pub struct Server {
@@ -24,7 +26,9 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            Request::try_from(&buffer[..]);
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
